cbeaa1822ef831017a703ba8b4c20d3bf9de48ce
Hirchi <48589267+Hirchi@users.noreply.github.com>
2021-10-06 23:30:23 +0200

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 data_url = "https://www.dropbox.com/s/0gac54ifqvnqlrl/full_2020%20%281%29.csv?dl=1"
-@st.cache
+
 def load_data(url):
     df = pd.read_csv(url)[:100]
     return df
