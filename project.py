34d6b22b8d5a24f7559655de8e41379d22f68a37
Hirchi <48589267+Hirchi@users.noreply.github.com>
2021-10-07 13:59:30 +0200

    Add files via upload

--- project.py
+++ project.py
@@ -1,11 +1,15 @@
-
+from matplotlib import pyplot as plt
 import streamlit as st
+import numpy as np
 import pandas as pd
+import altair as alt
+import time
 
 data_url = "https://www.dropbox.com/s/0gac54ifqvnqlrl/full_2020%20%281%29.csv?dl=1"
-
+@st.cache(max_entries=10, ttl=3600)
 def load_data(url):
     df = pd.read_csv(url)[:100]
     return df
 df = load_data(data_url)
 st.write(df.head())
+
